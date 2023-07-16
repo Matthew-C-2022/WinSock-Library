22d02b7971e37ec6a93997dbecd98560585362c0
Matthew-C-2022 <rawrmum@gmail.com>
2023-07-16 12:19:56 +0100

    Update

--- WinSockLib/TCP/TCPSClient.hpp
+++ WinSockLib/TCP/TCPSClient.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <iostream>
 #include <winsock2.h>
-#include <unordered_map>
 #pragma comment(lib, "ws2_32.lib")
 
 class SClient
--- WinSockLib/UDP/UDPSClient.hpp
+++ WinSockLib/UDP/UDPSClient.hpp
@@ -1,7 +1,5 @@
 #pragma once
-#include <iostream>
 #include <winsock2.h>
-#include <unordered_map>
 #pragma comment(lib, "ws2_32.lib")
 
 class SClient
